03c63b9711aa798edec5ff4b261a96921b64f654
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-10-24 22:23:13 -0300

    Final A2

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,19 @@
+﻿Musica musica1 =  new Musica();
+musica1.Nome = "Blinding Lights";
+musica1.Artista = "The Weeknd";
+musica1.Duracao = 199;
+musica1.Disponivel = true;
+//Console.WriteLine(musica1.Disponivel);
+Console.WriteLine(musica1.DescricaoResumida);
+
+Musica musica2 = new Musica();
+musica2.Nome = "Higher Power";
+musica2.Artista = "Coldplay";
+musica2.Duracao = 209;
+musica2.Disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+//musica1.ExibirNomeEArtista();
+//musica2.ExibirNomeEArtista();
